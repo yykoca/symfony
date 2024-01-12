99fcbd37a820dff3537f74cd50f0105ad8164e29
Yahya Yasir Koca <yykoca.08@gmail.com>
2024-01-12 10:23:05 +0100

    Add maker bundle and first controller

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    #[Route('/{max}', name: 'app_lucky_number')]
+    public function index(int $max): Response
+    {
+        $number = random_int(0, $max);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
